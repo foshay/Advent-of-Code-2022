553ce2000bf6688dbaf250cf34ea39607a9789cb
Isak Foshay <25090017+foshay@users.noreply.github.com>
2022-12-06 16:09:04 -0800

    Day 06 'I was drunk last night'

---
+++ 06/sol1.py
@@ -0,0 +1,19 @@
+with open("input.txt","r") as fp:
+    x = fp.readline().rstrip()
+    lookback = {}
+    count = 0
+    i=0
+    while i < len(x):
+        print(lookback)
+        if x[i] in lookback:
+            i = lookback[x[i]]+1
+            lookback = {}
+            count = 0
+        else:
+            lookback[x[i]] = i
+            count +=1
+            i+=1
+        if count == 4:
+            print(i)
+            exit()
+        
