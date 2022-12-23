34efc4c14f7a83ef0adef04c09bd4be648d2b956
isakteamjck <73260693+isakteamjck@users.noreply.github.com>
2022-12-23 12:41:13 -0800

    Day 13 pt2 done. Jank answer unsure why nested lists

--- 13/sol2.py
+++ 13/sol2.py
@@ -10,64 +10,81 @@ def main():
             a = eval(a)
             b = eval(b)
             #print(a,b)
-            print("Compare",a,'\/',b)
-            if(compare(a,b)==1): 
-                print(a,b,'lgtm')
-                a = str(a)
-                a = ''.join(i for i in a if not i in ('[',']',',',''))
-                b = str(b)
-                b = ''.join(i for i in b if not i in ('[',']',',',''))
-                packets.append(list(a.split(' ')))
-                packets.append(list(b.split(' ')))
-            else:
-                a = str(a)
-                a = ''.join(i for i in a if not i in ('[',']',',',''))
-                b = str(b)
-                b = ''.join(i for i in b if not i in ('[',']',',',''))
-                packets.append(list(b.split(' ')))
-                packets.append(list(a.split(' ')))
-                print(a,b,'wrong')
-        for i in packets:
-            for j in i:
-                j = int(j)
+            #print("Compare",a,'\/',b)
+            packets.append(a)
+            packets.append(b)
+            #if(compare(a,b)==1): 
+                #print(a,b,'lgtm')
+            #    packets.append(a)
+            #    packets.append(b)
+            #else:
+            #    packets.append(b)
+            #   packets.append(a)
+                #print(a,b,'wrong')
+        #for i in packets:
+        #    for j in i:
+        #        j = int(j)
         packets.append([[2]])
         packets.append([[6]])
-        print(packets)
-        packets.sort(key=lambda x: x[0])
-        
-        decoder=1
-        for i in range(len(packets)):
-            print(packets[i])
-            if packets[i] in ([[2]],[[6]]):
+        for i in packets:
+            print(i)
+        print('sorting')
+        final = []
+        while True:
+            i = len(packets)
+            if i == 1:
+                final.append(packets[0])
+                break
+            smallest = 0
+            for j in range(i):
+                if compare(packets[smallest],packets[j]) == -1:
+                    #print('new smallest',packets[j])
+                    smallest = j
+            final.append(packets.pop(smallest))
+            #print(final)
+        print('final---------------')
+        decoder = 1
+        for i in range(len(final)):
+            print(final[i])
+            if final[i] in ([[[[2]]]],[[[[6]]]]):
+                print('found',i)
                 decoder=decoder*(i+1)
-                print('found',decoder)
-        print(decoder)
+        print('decoder:',decoder)
+        #packets.sort(key=lambda x: x[0])
+        
+        #decoder=1
+        #for i in range(len(packets)):
+        #    print(packets[i])
+        #    if packets[i] in ([[2]],[[6]]):
+        #        decoder=decoder*(i+1)
+        #        print('found',decoder)
+        #print(decoder)
 def compare(a,b):
     for i in range(len(a)):
         if i>= len(b):
             return -1
         if type(a[i]) == int and type(b[i]) == int:
             if a[i] > b[i]:
-                print('int and int, WRONG',a[i],b[i])
+                #print('int and int, WRONG',a[i],b[i])
                 return -1
             elif a[i] < b[i]:
-                print('int and int, RIGHT',a[i],b[i])
+                #print('int and int, RIGHT',a[i],b[i])
                 return 1
         elif type(a[i]) == list and type(b[i]) == list:
-            print('list and list',a[i],b[i])
+            #print('list and list',a[i],b[i])
             res = compare(a[i],b[i])
             if res == -1:
                 return -1
             elif res == 1:
                 return 1
             if len(a[i]) < len(b[i]):
-                print('left len less than right len', len(a[i]),len(b[i]))
+                #print('left len less than right len', len(a[i]),len(b[i]))
                 return 1
         elif type(a[i]) == int or type(b[i]) == int:
-            print('list and int',a[i],b[i])
+            #print('list and int',a[i],b[i])
             if type(a[i]) == int: a[i] = [a[i]]
             else: b[i] = [b[i]]
-            print('converted',a[i],b[i])
+            #print('converted',a[i],b[i])
             res = compare(a[i],b[i])
             if res == -1:
                 return -1
@@ -85,7 +102,7 @@ def compare(a,b):
     #print("lgtm")
     if len(a) < len(b):
         return 1
-    print('lists same len',a,b)
+    #print('lists same len',a,b)
     return 0
 if __name__=="__main__":
     main()
