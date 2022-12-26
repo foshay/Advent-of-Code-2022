6d23b0163b304b07484beb824eee6571fd3fc161
isak <isakfoshay@pm.me>
2022-12-25 19:38:56 -0800

    Boundaries not the entire thing. Also remember to mult by 4mil

--- 15/sol2.py
+++ 15/sol2.py
@@ -20,31 +20,48 @@ def main():
                 delta = (dif-abs(i))
                 low = Sx-delta
                 if low < 0: low = 0
-                high = Sx+1+delta
+                high = Sx+delta
                 if high > bounds: high = bounds
                 if (Sy+i) <= bounds and (Sy+i >= 0):
-                    print("Checking",(Sy+i))
+                    #print("Checking",(Sy+i))
                     if (Sy+i) in grid:
-                        grid[(Sy+i)].extend(list(range(low,high)))
+                        grid[(Sy+i)].extend([[low,high]])
                     else:
-                        grid[(Sy+i)] = list(range(low,high))
+                        grid[(Sy+i)] = [[low,high]]
+                    #print(grid[Sy+i])
                     grid[Sy+i].sort()
                     grid[Sy+i] = list(k for k,_ in itertools.groupby(grid[Sy+i]))
                 #print(grid[(Sy+i)])
                     #if By <= bounds and By in grid and Bx in grid[By]:
                     #    grid[By].remove(Bx)
-        lenNormal = 0
-        if len(grid[0]) == len(grid[1]):
-            lenNormal = len(grid[0])
-        else:
-            lenNormal = min(len(grid[0]),len(grid[1]))
-        print(lenNormal)
-        for i in range(0,bounds+1):
-            if len(grid[i]) != lenNormal:
-                for j in range(0,bounds+1):
-                    if j not in grid[i]:
-                        print('answer',j*4000000 +i)
-                        exit()
+        #lenNormal = 0
+        #if len(grid[0]) == len(grid[1]):
+        #    lenNormal = len(grid[0])
+        #else:
+        #    lenNormal = min(len(grid[0]),len(grid[1]))
+        #print(lenNormal)
+        for i in range(bounds+1):
+            #print(grid[i][0][0], grid[i][1][0])
+            if grid[i][0][0] == grid[i][1][0]:
+                grid[i].pop(0)
+            #print('')
+        for i in range(bounds+1):
+            #print(i,grid[i])
+            start = grid[i][0]
+            for j in range(1,len(grid[i])):
+                #print(grid[i])
+                if grid[i][j][0] <= start[1]+1:
+                    x = min(start[0], grid[i][j][0])
+                    y = max(start[1], grid[i][j][1])
+                else:
+                    print(grid[i][j],start,'\nAnswer:',str((start[1]+1)*4000000+i))
+                    grid[i] = start
+                    break
+                start = [x,y]
+                #print(start)
+            #print(start)
+            grid[i] = start
+                    #print(grid[i][j],grid[i][j+1])
         #print('Answer:',len(grid[target]))
 
 if __name__=="__main__":
